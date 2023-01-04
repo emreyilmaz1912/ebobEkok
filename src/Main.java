47c30b5808c1a3f7f38c7dd49f412097e3933b7c
emreyilmaz1912 <emreyilmaz264@gmail.com>
2023-01-04 22:17:10 +0300

    ilk commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int n1, n2, ebob = 1, ekok = 1, i = 1, j = 1;
+
+        System.out.print("1.sayiyi giriniz: ");
+        n1 = input.nextInt();
+
+        System.out.print("2.sayiyi giriniz: ");
+        n2 = input.nextInt();
+
+        if (n1 < n2) {
+            while (i <= n1) {
+                if (n1 % i == 0 && n2 % i == 0) {
+                    ebob = i;
+                }
+                i++;
+            }
+        } else {
+            while (i <= n2) {
+                if (n1 % i == 0 && n2 % i == 0) {
+                    ebob = i;
+                }
+                i++;
+            }
+        }
+        System.out.println("Sayıların Ebobu : " + ebob);
+        System.out.print("");
+
+        while (n1 * n2 >= j) {
+            if (j % n1 == 0 && j % n2 == 0) {
+                ekok = j;
+                break;
+            }
+            j++;
+        }
+        System.out.print("Sayıların Ekoku : " + ekok);
+    }
+}
